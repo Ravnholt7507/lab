9134cec0c1ebb347355cfffb1c19770df72e6abf
Ravnholt7507 <Andreas.ravnholt99@gmail.com>
2022-10-19 15:10:06 +0200

    errorcatching + cleanup

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -15,13 +15,13 @@ outputfile = ">> popperoutput.txt"
 if file_path != '':
     with open('popperoutput.txt', 'w') as ofile:
         ofile.write('')
+
     for current_folder in os.listdir(file_path):
         f = os.path.join(file_path, current_folder)
         if current_folder == ".DS_Store":
             continue
         with open('popperoutput.txt', 'a') as ofile:
-            ofile.write(current_folder + ":")
-            ofile.write('\n')
+            ofile.write("ACTION:"+current_folder +'\n')
 
         os.system(poppercommand + " " + f + "/100" + " " + outputfile)
 else:
