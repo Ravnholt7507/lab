4cd6465732fc3f049cee12b29398c9c9d582ff3b
Ravnholt7507 <Andreas.ravnholt99@gmail.com>
2022-10-20 14:35:17 +0200

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -6,23 +6,16 @@ root = tk.Tk()
 root.withdraw()
 
 file_path = askdirectory(title="select folder")
-
 poppercommand = "python3 popper.py"
-
 outputfile = ">> popperoutput.txt"
 
 
 if file_path != '':
-    with open('popperoutput.txt', 'w') as ofile:
-        ofile.write('')
-
     for current_folder in os.listdir(file_path):
         f = os.path.join(file_path, current_folder)
         if current_folder == ".DS_Store":
             continue
-        with open('popperoutput.txt', 'a') as ofile:
-            ofile.write("ACTION:"+current_folder +'\n')
 
-        os.system(poppercommand + " " + f + "/100" + " " + outputfile)
+        os.system(poppercommand + " " + f + "/100" + " > "+"output/"+current_folder+".txt")
 else:
     print("Error: Please select a folder")
