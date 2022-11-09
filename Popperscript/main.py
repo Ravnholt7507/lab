005cb48b9fca3b1eabbf0c17904bebc452ceab59
Ravnholt7507 <Andreas.ravnholt99@gmail.com>
2022-11-09 11:40:36 +0100

    Update main.py

--- Popperscript/main.py
+++ Popperscript/main.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from tkinter.filedialog import askdirectory
+import fileinput
 
 root = tk.Tk()
 root.withdraw()
@@ -23,3 +24,33 @@ if file_path != '':
         os.system(poppercommand + " " + f + "/100" + " --timeout 10000" + " " + outputFile)
 else:
     print("Error: Please select a folder")
+
+#format popper-output.txt in order to fit fast-downward
+get_head = []
+with open("popper-output.txt", "r") as file :
+    file_data = file.read()
+    while not get_head.__contains__(")"):
+        get_head = file.read(1)
+file_data = file_data.replace("),", ");")
+file_data = file_data.replace("_", ":")
+file_data = file_data.replace(",E", "")
+
+#change variables from A to ?A as that is what our planner expects in order to identify variables
+refactor_variables = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "Y", "Q", "Z", "X"]
+replaces = ["?A", "?B", "?C", "?D", "?E", "?F", "?G", "?H", "?I", "?J", "?K", "?L", "?M", "?N", "?O", "?P", "?Q", "?R", "?S", "?T", "?V", "?W", "?Y", "?Q", "?Z", "?X"]
+
+i = 0
+for letter in refactor_variables:
+    file_data = file_data.replace(letter, replaces[i])
+    i += 1
+
+
+
+
+with open("popper-output.txt", "w") as file:
+    file.write(file_data)
+
+
+
+
+
